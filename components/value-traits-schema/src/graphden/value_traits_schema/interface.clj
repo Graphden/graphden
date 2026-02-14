8288e87db2338f8c75c4e0cd8f7f824be3f85d3d
izpa <ispius.prime@gmail.com>
2026-02-14 04:46:05 +0300

    fix

---
+++ components/value-traits-schema/src/graphden/value_traits_schema/interface.clj
@@ -0,0 +1,104 @@
+(ns graphden.value-traits-schema.interface
+  "Value traits schema definition.
+
+   Extends graph-data-schema with entities for assigning traits to arg-values:
+   - trait: definition of a trait (name, description)
+   - value-trait: association between arg-value and trait
+
+   Traits allow marking values with special behaviors:
+   - merge-protected: value should not be transferred during branch merge
+   - encrypted: value should be stored encrypted (future)
+   - sensitive: value should be masked in UI (future)
+
+   Addons can define their own traits and assign them to values."
+  (:require
+    [graphden.data-schema-protocol.interface :as ds]))
+
+
+;; === Stable UUIDs for value traits schema elements ===
+;;
+;; See graph-data-schema for explanation of UUID stability.
+;; IMPORTANT: Never change these UUIDs!
+
+;; Entity UUIDs
+;; NOTE: These must be unique across all schema extensions!
+;; Previously used UUIDs that conflicted with versioned-data-schema have been replaced.
+(def ^:private trait-entity-uuid
+  #uuid "71727374-7576-4a7b-8c9d-0e1f2a3b4c5d")
+
+
+(def ^:private value-trait-entity-uuid
+  #uuid "72737475-7677-4b8c-9d0e-1f2a3b4c5d6e")
+
+
+;; Field UUIDs for :trait
+(def ^:private trait-name-field-uuid
+  #uuid "73747576-7778-4c9d-0e1f-2a3b4c5d6e7f")
+
+
+(def ^:private trait-description-field-uuid
+  #uuid "74757677-7879-4d0e-1f2a-3b4c5d6e7f8a")
+
+
+;; Field UUIDs for :value-trait
+(def ^:private value-trait-arg-value-id-field-uuid
+  #uuid "75767778-7980-4e1f-2a3b-4c5d6e7f8a9b")
+
+
+(def ^:private value-trait-trait-id-field-uuid
+  #uuid "76777879-8081-4f2a-3b4c-5d6e7f8a9b0c")
+
+
+;; Well-known trait UUIDs (for seeding)
+(def merge-protected-trait-uuid
+  "UUID for the merge-protected trait. Use this when seeding or querying."
+  #uuid "11111111-1111-4111-8111-111111111111")
+
+
+(defn extend-builder
+  "Extends a builder with value traits entities.
+   Returns the builder for further extension or finalization.
+
+   Expects :arg-value entity to be defined (from graph-data-schema)."
+  [builder]
+  (-> builder
+      ;; trait: definition of a trait
+      (ds/add-entity :trait trait-entity-uuid
+                     {:name {:uuid trait-name-field-uuid
+                             :type :text}
+                      :description {:uuid trait-description-field-uuid
+                                    :type :text
+                                    :nullable? true}})
+      (ds/add-constraint :trait {:type :unique :fields [:name]})
+
+      ;; value-trait: association between arg-value and trait
+      (ds/add-entity :value-trait value-trait-entity-uuid
+                     {:arg-value-id {:uuid value-trait-arg-value-id-field-uuid
+                                     :type :ref :ref-entity :arg-value}
+                      :trait-id {:uuid value-trait-trait-id-field-uuid
+                                 :type :ref :ref-entity :trait}})
+      (ds/add-constraint :value-trait {:type :unique :fields [:arg-value-id :trait-id]})))
+
+
+(defn seed-traits!
+  "Seeds the standard traits into storage. Idempotent.
+   Call this after initializing storage with the schema."
+  [storage]
+  (let [sp (requiring-resolve 'graphden.storage-protocol.interface/create-entity)
+        read-entity (requiring-resolve 'graphden.storage-protocol.interface/read-entity)]
+    ;; Only seed if not exists
+    (when-not (@read-entity storage :trait merge-protected-trait-uuid)
+      (@sp storage :trait
+           {:id merge-protected-trait-uuid
+            :name "merge-protected"
+            :description "Value will not be transferred during branch merge"}))))
+
+
+(def trait-entities
+  "Set of entity names that are trait-specific."
+  #{:trait :value-trait})
+
+
+(def well-known-traits
+  "Map of well-known trait names to their UUIDs."
+  {:merge-protected merge-protected-trait-uuid})
---
+++ components/value-traits-schema/test/graphden/value_traits_schema/interface_test.clj
@@ -0,0 +1,144 @@
+(ns graphden.value-traits-schema.interface-test
+  (:require
+    [clojure.test :refer [deftest is testing]]
+    [graphden.data-schema-protocol.interface :as ds]
+    [graphden.graph-data-schema.interface :as gds]
+    [graphden.malli-data-schema.interface :as mds]
+    [graphden.memory-storage.interface :as mem]
+    [graphden.storage-protocol.interface :as sp]
+    [graphden.value-traits-schema.interface :as vts]))
+
+
+(defn- create-test-storage
+  "Creates storage with graph + traits schema."
+  []
+  (let [schema (-> (mds/create-builder)
+                   (gds/extend-builder)
+                   (vts/extend-builder)
+                   (ds/build))
+        storage (mem/create-storage)]
+    (sp/initialize-with-cleanup! storage schema)))
+
+
+(deftest schema-builds-test
+  (testing "schema with traits builds successfully"
+    (let [schema (-> (mds/create-builder)
+                     (gds/extend-builder)
+                     (vts/extend-builder)
+                     (ds/build))]
+      (is (some? schema))
+      (is (contains? (set (ds/entities schema)) :trait))
+      (is (contains? (set (ds/entities schema)) :value-trait)))))
+
+
+(deftest trait-crud-test
+  (testing "can create and read traits"
+    (let [storage (create-test-storage)
+          trait (sp/create-entity storage :trait
+                                  {:name "test-trait"
+                                   :description "A test trait"})]
+      (is (uuid? (:id trait)))
+      (is (= "test-trait" (:name trait)))
+      (is (= "A test trait" (:description trait)))
+      (sp/close storage)))
+
+  (testing "trait name is unique"
+    (let [storage (create-test-storage)]
+      (sp/create-entity storage :trait {:name "unique-trait"})
+      (is (thrown? Exception
+            (sp/create-entity storage :trait {:name "unique-trait"})))
+      (sp/close storage))))
+
+
+(deftest value-trait-crud-test
+  (testing "can assign trait to arg-value"
+    (let [storage (create-test-storage)
+          ;; Create fn-schema and arg-schema first
+          fn-schema (sp/create-entity storage :fn-schema
+                                      {:name "test-schema"
+                                       :returned-type :text})
+          arg-schema (sp/create-entity storage :arg-schema
+                                       {:fn-schema-id (:id fn-schema)
+                                        :name "password"
+                                        :type :text
+                                        :required true})
+          ;; Create arg-value
+          arg-value (sp/create-entity storage :arg-value
+                                      {:arg-schema-id (:id arg-schema)
+                                       :value "secret123"})
+          ;; Create trait and assign
+          trait (sp/create-entity storage :trait
+                                  {:name "secret"
+                                   :description "Should be hidden"})
+          value-trait (sp/create-entity storage :value-trait
+                                        {:arg-value-id (:id arg-value)
+                                         :trait-id (:id trait)})]
+      (is (uuid? (:id value-trait)))
+      (is (= (:id arg-value) (:arg-value-id value-trait)))
+      (is (= (:id trait) (:trait-id value-trait)))
+      (sp/close storage)))
+
+  (testing "same trait cannot be assigned twice to same value"
+    (let [storage (create-test-storage)
+          fn-schema (sp/create-entity storage :fn-schema
+                                      {:name "test-schema"
+                                       :returned-type :text})
+          arg-schema (sp/create-entity storage :arg-schema
+                                       {:fn-schema-id (:id fn-schema)
+                                        :name "x"
+                                        :type :text
+                                        :required true})
+          arg-value (sp/create-entity storage :arg-value
+                                      {:arg-schema-id (:id arg-schema)
+                                       :value "test"})
+          trait (sp/create-entity storage :trait {:name "my-trait"})]
+      (sp/create-entity storage :value-trait
+                        {:arg-value-id (:id arg-value)
+                         :trait-id (:id trait)})
+      (is (thrown? Exception
+            (sp/create-entity storage :value-trait
+                              {:arg-value-id (:id arg-value)
+                               :trait-id (:id trait)})))
+      (sp/close storage))))
+
+
+(deftest query-traits-test
+  (testing "can query traits for a value"
+    (let [storage (create-test-storage)
+          fn-schema (sp/create-entity storage :fn-schema
+                                      {:name "test-schema"
+                                       :returned-type :text})
+          arg-schema (sp/create-entity storage :arg-schema
+                                       {:fn-schema-id (:id fn-schema)
+                                        :name "val"
+                                        :type :text
+                                        :required true})
+          arg-value (sp/create-entity storage :arg-value
+                                      {:arg-schema-id (:id arg-schema)
+                                       :value "myval"})
+          trait1 (sp/create-entity storage :trait {:name "trait-a"})
+          trait2 (sp/create-entity storage :trait {:name "trait-b"})
+          _ (sp/create-entity storage :value-trait
+                              {:arg-value-id (:id arg-value)
+                               :trait-id (:id trait1)})
+          _ (sp/create-entity storage :value-trait
+                              {:arg-value-id (:id arg-value)
+                               :trait-id (:id trait2)})
+          value-traits (sp/query-entities storage :value-trait
+                                          {:arg-value-id (:id arg-value)})]
+      (is (= 2 (count value-traits)))
+      (is (= #{(:id trait1) (:id trait2)}
+             (set (map :trait-id value-traits))))
+      (sp/close storage))))
+
+
+(deftest well-known-traits-test
+  (testing "well-known traits are defined"
+    (is (uuid? vts/merge-protected-trait-uuid))
+    (is (= vts/merge-protected-trait-uuid
+           (:merge-protected vts/well-known-traits)))))
+
+
+(deftest trait-entities-test
+  (testing "trait-entities set is correct"
+    (is (= #{:trait :value-trait} vts/trait-entities))))
